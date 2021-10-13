fc7c83f2e29f68a828cf671547c39fe93f97ac99
Matheus Santiago <matheussantiagopmw@gmail.com>
2021-10-13 16:35:44 -0300

    Consumindo api do stripe com ServerSideRendering

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,7 +1,10 @@
 import styles from './styles.module.scss';
 
-export function SubscribeButton() {
-    
+interface SubscribeButtunProps {
+    priceId: string;
+}
+
+export function SubscribeButton({priceId}: SubscribeButtunProps) {
     return (
         <button 
         type="button"
