99a3c487115588cf1a3de2c7e8a75e3ecd51c8ee
Matheus Santiago <matheussantiagopmw@gmail.com>
2021-10-18 13:59:49 -0300

    Redirecionando para o stripe

--- src/components/SingInButton/index.tsx
+++ src/components/SingInButton/index.tsx
@@ -7,7 +7,7 @@ import styles from './styles.module.scss';
 
 export function SingInButton() {
     const [session] = useSession();
-    console.log(session)
+    
     return session ? (
         <button 
         type="button"
