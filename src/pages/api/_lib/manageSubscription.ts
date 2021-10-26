a02bbeac2c84ef97354c47b6041aa6b8f61eaa2a
Matheus Santiago <matheussantiagopmw@gmail.com>
2021-10-26 17:10:57 -0300

    Salvando os dados do evento no faunadb

---
+++ src/pages/api/_lib/manageSubscription.ts
@@ -0,0 +1,39 @@
+import { query as q } from 'faunadb'
+
+import { fauna } from '../../../services/fauna'
+import { stripe } from '../../../services/stripe'
+  //buscar o usuárioo no banco do FaunaDB com o Id {customerId}
+  //Salvar os dados da subscription no fauna 
+export async function saveSubscription(
+  subscriptionId: string,
+  customerId: string,
+  ) {
+
+    const userRef = await fauna.query(//rertornando o ref(id) do user id do fauna que é igual ao customerId
+      q.Select(
+        'ref',
+        q.Get(
+          q.Match(
+            q.Index('user_by_stripe_customer_id'),
+            customerId
+          )
+        )
+      )
+    )
+
+    const subscription = await stripe.subscriptions.retrieve(subscriptionId)//buscando todos os dados da subscription
+
+    const subscriptionData = {
+      id: subscription.id,
+      userId: userRef,
+      status: subscription.status,
+      price_id: subscription.items.data[0].price.id,
+    }
+    
+    await fauna.query(
+      q.Create(
+        q.Collection('subscriptions'),
+        { data: subscriptionData }
+      )
+    )
+  }
