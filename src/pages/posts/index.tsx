cccd1536623f1daab113b12f8a3c215c8a386e13
Matheus Santiago <matheussantiagopmw@gmail.com>
2021-11-05 18:07:33 -0300

    Listando os posts e transformando os dados via SSR

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,11 +1,25 @@
 import { GetStaticProps } from 'next';
 import Head from 'next/head';
 import React from 'react';
-import { getPrismicClient } from '../../services/prismic';
-import styles from './styles.module.scss';
 import Prismic from '@prismicio/client';
+import { RichText } from 'prismic-dom'
 
-export default function Posts() {
+import { getPrismicClient } from '../../services/prismic';
+
+import styles from './styles.module.scss';
+
+type Post = {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updatedAt: string;
+}
+
+interface PostsProps {
+  posts: Post[]
+}
+
+export default function Posts({ posts }: PostsProps) {
   return(
     <>
       <Head>
@@ -14,21 +28,13 @@ export default function Posts() {
 
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a href="#">
-            <time>03 de novembro de 2021</time>
-            <strong>Introdução a ciclo de vida de software</strong>
-            <p>De acordo com Gordon e Gordon (2006), o ciclo de vida do desenvolvimento de sistemas (Systems Development Life Cycle – SDLC), conhecido também como ciclo de vida do software, refere-se aos estágios de concepção, projeto, criação e implementação de um Sistema de Informação (SI). Um desdobramento possível para SDLC </p>
+          { posts.map(post => (
+            <a key={post.slug} href="#">
+            <time>{post.updatedAt}</time>
+            <strong>{post.title}</strong>
+            <p>{post.excerpt}</p>
           </a>
-          <a href="#">
-            <time>03 de novembro de 2021</time>
-            <strong>Introdução a ciclo de vida de software</strong>
-            <p>De acordo com Gordon e Gordon (2006), o ciclo de vida do desenvolvimento de sistemas (Systems Development Life Cycle – SDLC), conhecido também como ciclo de vida do software, refere-se aos estágios de concepção, projeto, criação e implementação de um Sistema de Informação (SI). Um desdobramento possível para SDLC </p>
-          </a>
-          <a href="#">
-            <time>03 de novembro de 2021</time>
-            <strong>Introdução a ciclo de vida de software</strong>
-            <p>De acordo com Gordon e Gordon (2006), o ciclo de vida do desenvolvimento de sistemas (Systems Development Life Cycle – SDLC), conhecido também como ciclo de vida do software, refere-se aos estágios de concepção, projeto, criação e implementação de um Sistema de Informação (SI). Um desdobramento possível para SDLC </p>
-          </a>
+          ))}
         </div>
       </main>
     </>
@@ -45,9 +51,22 @@ export const getStaticProps: GetStaticProps = async () => {
     pageSize: 100,
   })
 
-  console.log(response)
+  const posts = response.results.map(post => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      excerpt: post.data.content.find(content => content.type === 'paragraph')?.text ?? '',
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric'
+      })
+    }
+  })
 
   return {
-    props: {}
+    props: {
+      posts
+    }
   }
 }
