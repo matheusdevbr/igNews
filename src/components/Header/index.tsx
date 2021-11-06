7141837d419e4492eb93191f79c2c8cddfa9d3c4
Matheus Santiago <matheussantiagopmw@gmail.com>
2021-11-06 16:40:36 -0300

    Conceito de SPA no next. Link Component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link'
+
 import { SingInButton } from '../SingInButton';
 import styles from './styles.module.scss';
 
@@ -7,8 +9,12 @@ export function Header() {
             <div className={styles.headerContent}>
                 <img src="/images/logo.svg" alt="ig.news" />
                 <nav>
+                    <Link href="/">
                     <a className={styles.active}>Home</a>
-                    <a>Posts</a>
+                    </Link>
+                    <Link href="/posts">
+                        <a>Posts</a>
+                    </Link>
                 </nav>
 
                 <SingInButton />
